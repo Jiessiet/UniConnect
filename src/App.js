a404c3acb40331de2e8b5912a6a02e24c726a390
Onur Eren Arpacı <onurerenarpaci@Onurs-MacBook-Pro.local>
2021-10-24 00:49:14 -0400

    basic folder structure and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import LandingPage from "./views/LandingPage/LandingPage"
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => <LandingPage />} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
