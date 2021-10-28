ba4ba88913d575f84c5b153df3985b87c81d1ff5
tiechu <jiessie.tie@mail.utoronto.ca>
2021-10-28 17:12:02 -0400

    added home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core';
 
 import Home from './views/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage:'url("/images/Online-pana.png")',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundColor: '#606c38'
+  }
+
+}));
+
+export default function App(){
+  const classes = useStyles();
   return (
-    <div className='App'>
+    // <div className='App'>
+    <div className ={classes.root}>;
       <BrowserRouter>
         <Container maxidth='lg'>
           <Navbar />
@@ -18,6 +32,4 @@ function App() {
       </BrowserRouter>
     </div>
   );
-}
-
-export default App;
+};
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,14 +1,74 @@
-import React from 'react'
-import useStyles from "./styles";
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
 
-const Home = () => {
+import SearchBar from "material-ui-search-bar";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'top',
+    height: '100vh',
+    fontFamily: 'Nunito',
+  },
+  appbar: {
+    background: 'none',
+  },
+  appbarWrapper: {
+    width: '80%',
+    margin: '0 auto',
+  },
+  appbarTitle: {
+    flexGrow: '1',
+  },
+  colorText: {
+    color: '#d4e09b',
+  },
+  container: {
+    textAlign: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: '4rem',
+  },
+}));
+
+export default function Home() {
   const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  }, []);
   return (
-    <div>
-      Hello
-    </div>
-  )
+   <div className={classes.root}>
+    <AppBar className ={classes.appbar} elevation={0}>
+      <Toolbar className={classes.appbarWrapper}> 
+      <h1 className={classes.appbarTitle}>UniConnect</h1>
+      <IconButton>
+      </IconButton>
+      </Toolbar>
+    </AppBar>
+
+    <Collapse in={checked}
+        {...(checked ? { timeout: 1000 } : {})}
+        collapsedHeight={50}
+      >
+      <div className={classes.container}>
+        <h1 className={classes.title}> 
+        Welcome to <br /> <span className={classes.colorText}>UniConnect</span>
+        </h1>
+        <SearchBar
+      onChange={() => console.log('onChange')}
+      onRequestSearch={() => console.log('onRequestSearch')}
+      style={{
+        margin: '0 auto',
+        maxWidth: 800
+      }}
+    />
+      </div>
+    </Collapse>
+
+  </div>
+  );
 }
-
-export default Home
-
