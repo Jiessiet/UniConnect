84e83403b0fb2c14bc415b360a8b8cce0a1ed40d
Suhyun Park <aodlfduddj@gmail.com>
2021-10-28 23:43:51 -0400

    add basic navbar and styles

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,5 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-
+    appbar: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px 30px',
+    },
+    toolbar: {
+        display: 'flex',
+    },
+    logoImage: {
+        width: '100px'
+    }
 }));
