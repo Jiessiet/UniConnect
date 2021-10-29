e4e20f6b230e46b8870c01241673391989693f81
Mizna <mizna.dada@mail.utoronto.ca>
2021-10-29 16:40:07 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import Home from './views/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import Login from './views/Login/login.js'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Navbar />
           <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
           </Switch>
         </Container>
       </BrowserRouter>
