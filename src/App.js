9f4c10461eb5d64472ffd70471f4de6c33d49299
Mizna <mizna.dada@mail.utoronto.ca>
2021-10-29 16:52:52 -0400

    added routing for signup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import Home from './views/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import Signup from './views/Signup/signup'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Navbar />
           <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/signup" exact component={Signup} />
           </Switch>
         </Container>
       </BrowserRouter>
