9c0e459719fd78d24abd3869bbf403cbd2ef5d08
Mizna <mizna.dada@mail.utoronto.ca>
2021-10-28 21:06:09 -0400

    Added Forgot Password and Sign In links

--- src/views/Login/login.js
+++ src/views/Login/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import loginImage from "./loginImage.svg";
 import background from "./greenbg.jpg"
-import { Button, Grid, Paper, Avatar, Typography, Box, TextField} from '@mui/material';
+import { Button, Grid, Paper, Avatar, Typography, Box, TextField, Link} from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
 
 export class login extends React.Component {
@@ -13,13 +13,13 @@ render() {
         spacing={0}
         padding = '0px'
         padding-left='0px'
-        direction="column"
+        direction="row"
         alignItems="center"
         justifyContent="center"
         style={{ minHeight: '100vh', minWidth: '100vw'}}
         sx={{
             backgroundImage: `url(${background})`,
-            backgroundSize: 'cover', 
+            backgroundSize: 'cover',
             backgroundPosition: 'center'}}>
         <Grid container 
             direction="row" 
@@ -29,7 +29,7 @@ render() {
             borderRadius= {16}
             padding= '0px'
             xs={6}>
-            <Grid item xs={6}>
+            <Grid item xs={6} alignItems='flex-end' >
                 <img src={loginImage}/>
             </Grid>
             <Grid item xs={5}>
@@ -37,14 +37,27 @@ render() {
                     {/* <Avatar sx={{}}>
                         <LoginIcon />
                     </Avatar> */}
-                    <Typography component = "h1" variant = 'h3' marginBottom = '0px' > Welcome Back to
+                    <Typography component = "h1" variant = 'h3' marginRight = '10px' > Welcome Back to
                     UniConnect! </Typography>
                     <form spacing={2}> 
                         <TextField fullWidth label='Username' margin = 'normal' placeholder='Enter Username'/>
                         <TextField fullWidth label='Password' marginBottom = 'normal' placeholder='Enter Password'/>
                     </form>
                     <Box className="footer"/>
-                        <Button variant="contained" sx={{mt: 1, background:'green'}}>Login</Button>
+                        <Button type = 'submit' variant="contained" sx={{mt: 1, background:'green'}}>Login</Button>
+            </Grid>
+            <Grid container direction='row' alignItems='flex-end' justifyContent='flex-end' padding='0' sx={{mt: 0, mb: 4, mr: 5, ml: 5}}>
+                <Grid xs={6}> </Grid>
+                <Grid item xs>
+                <Link href="#" variant="body2" color="#1fc449">
+                    Forgot password?
+                </Link>
+                </Grid>
+                <Grid item>
+                <Link href="#" variant="body2" color="#1fc449">
+                    {"Don't have an account? Sign Up"}
+                </Link>
+                </Grid>
             </Grid>
         </Grid>
     </Grid>)
