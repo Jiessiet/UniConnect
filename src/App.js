ee864a8355760dcf404c8005818c2f88bfdb3273
Mizna <mizna.dada@mail.utoronto.ca>
2021-10-29 16:35:12 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './views/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Timeline from './views/Timeline';
+import { createEvent } from './views/Event/create-event';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/timeline" exact component={Timeline} />
+            <Route path="/createEvent" exact component={createEvent}/>
           </Switch>
         </Container>
       </BrowserRouter>
