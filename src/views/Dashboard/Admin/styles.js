3bf8bbc2c99a2c406b90041b9f4050c6a70ad0bb
Suhyun Park <aodlfduddj@gmail.com>
2021-11-06 22:56:13 -0400

    added dashboard and editted profile

---
+++ src/views/Dashboard/Admin/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  card: {
+    width: '85%',
+    margin: '20px auto',
+    display: 'flex',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    padding: '16px 0',
+    justifyContent: 'center'
+  }
+}));
