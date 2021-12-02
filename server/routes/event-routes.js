09c8819fc7e6bfc4e18956665be8cd3e2cc0f4aa
Suhyun Park <aodlfduddj@gmail.com>
2021-12-02 14:07:05 -0500

    added some event routes

--- server/routes/event-routes.js
+++ server/routes/event-routes.js
@@ -1,9 +1,58 @@
-const express = require("express");
+const express = require('express');
+const { Event } = require('../mongodb/models/eventModel');
+const authenticate = require('../middleware/authmiddleware');
+const { mongoose } = require('../mongodb/config');
 
 const router = express.Router();
 
+// get all events
 router.get('/api/events', async (req, res) => {
+  try {
+    const event = await Event.find();
 
-})
+    res.status(200).json(event);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+// create an event
+router.post('/api/events', authenticate, async (req, res) => {
+  const newEvent = new Event({
+    ...req.body,
+    creator: req.user,
+    createAt: new Date().toISOString()
+  });
+
+  try {
+    await newEvent.save();
+
+    res.status(201).json(newEvent);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+});
+
+// update event
+router.patch('/api/events/:id', authenticate, async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No event with that id');
+  
+  const updatedEvent = await Event.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+  res.json(updatedEvent);
+});
+
+// delete event
+router.delete('/api/events/:id', authenticate, async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No event with that id');
+
+    await Event.findByIdAndDelete(id);
+
+    res.json({ message: 'Event deleted successfully' })
+});
 
 module.exports = router;
