0e32795447a963e0262b9ec517df1aa774a85547
Mizna <mizna.dada@mail.utoronto.ca>
2021-12-03 16:57:19 -0500

    Completed user routes and fixed user model error

--- server/mongodb/models/userModel.js
+++ server/mongodb/models/userModel.js
@@ -22,50 +22,46 @@ const UserSchema = new mongoose.Schema({
 		required: true,
 		minlength: 6
 	},
-	// username: {
-	// 	type: String,
-	// 	required: true,
-	// 	minlength: 5
-	// },
-	// userType: {
-	// 	type: Boolean,
-	// 	required: true
-	// },
-	// // userID: {
-	// // 	type: String,
-	// // 	required: true
-	// // },
-	// name: {
-	// 	type: String,
-	// 	required: true
-	// },
-	// profilePhoto: {
-	// 	data: Buffer,
-    //     contentType: String
-	// },
-	// description: {
-	// 	type: String,
-	// 	required: true
-	// },
-	// tags: [{
-	// 	type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'Tag'
-	// }],
-	// friends: [{
-	// 	type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'User',
-	// }],
-	// attendingEvents: [{
-	// 	type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'Event'
-	// }],
-	// university: {
-	// 	type: String,
-	// 	required: true
-	// },
-	// actionLog:[{
-	// 	type: String
-	// }]
+	username: {
+		type: String,
+		minlength: 5,
+		required: true
+	},
+	userType: {
+		type: Boolean,
+		default: false
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	profilePhoto: {
+		data: Buffer,
+        contentType: String
+	},
+	description: {
+		type: String,
+		default: ''
+	},
+	tags: [{
+		type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tag'
+	}],
+	friends: [{
+		type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+	}],
+	attendingEvents: [{
+		type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event'
+	}],
+	university: {
+		type: String,
+		required: true
+	},
+	actionLog:[{
+		type: String
+	}]
 })
 
 UserSchema.pre('save', function(next) {
